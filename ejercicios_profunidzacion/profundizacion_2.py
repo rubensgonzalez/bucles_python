3e1065230d0ddd0feeb4ac45f909b7b3cb652688
Ruben <87348517+rubensgonzalez@users.noreply.github.com>
2021-08-03 00:38:05 -0300

    Update profundizacion_2.py

--- ejercicios_profunidzacion/profundizacion_2.py
+++ ejercicios_profunidzacion/profundizacion_2.py
@@ -33,5 +33,37 @@ Se debe debe imprimir un cartel de error si el operador ingresado no es
 alguno de lo soportados o no es la palabra "FIN".
 '''
 
+op= True
+resultado = 0
+
+print("Para salir de ingresar como operador FIN")
+
+while (op != "FIN"):
+    n1 = int(input("Ingrese el primer numero\n"))
+    n2 = int(input("Ingrese el segundo numero\n"))
+    op = str(input("Ingrese el operador\n"))
+    
+    if op == "+":
+        resultado = n1 + n2
+        print("La operacion realizada es SUMA y el resultado es: {}\n".format(resultado))
+        continue
+    elif op == "-":
+        resultado = n1 - n2
+        print("La operacion realizada es RESTA y el resultado es: {}\n".format(resultado))
+        continue
+    elif op == "*":
+        resultado = n1 * n2
+        print("La operacion realizada es MULTIPLICACION y el resultado es: {}\n".format(resultado))
+        continue
+    elif op == "/":
+        resultado = n1 / n2
+        print("La operacion realizada es DIVISION y el resultado es: {}\n".format(resultado))
+        continue
+    elif op == "**":
+        resultado = n1 ** n2
+        print("La operacion realizada es Exponente/Potencia y el resultado es: {}\n".format(resultado))
+        continue
+
+
 print("Mi Calculadora (^_^)")
 # Empezar aquí la resolución del ejercicio
